1d2134621f0126f1fbc284e38febc201a7ccc1d0
davis-kane <davisjkane@gmail.com>
2017-11-03 15:36:49 -0400

    commit

---
+++ ex1.rb
@@ -0,0 +1,12 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "Here is another line."
+
+# This is a comment yo. This line will not run.
+
+puts "This line will run!"
